48b951a63ae5af05b035b1cc540b78904538e294
Fathi Hindi <fathe.hindi@gmail.com>
2017-05-19 10:34:56 +0300

    Adding auto generated JS files.

---
+++ pub/static/frontend/Magento/luma/ar_SA/Magento_SendFriend/back-event.js
@@ -0,0 +1,1 @@
+C:/wamp64/www/oneplace/vendor/magento/module-send-friend/view/frontend/web/back-event.js
---
+++ pub/static/frontend/Magento/luma/ar_SA/Magento_Theme/js/row-builder.js
@@ -0,0 +1,1 @@
+C:/wamp64/www/oneplace/vendor/magento/module-theme/view/frontend/web/js/row-builder.js
