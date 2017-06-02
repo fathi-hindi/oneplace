d84b41888bbaf9374299ee0ad6361810b80ab8d0
Fathi Hindi <fathe.hindi@gmail.com>
2017-06-02 14:51:05 +0300

    Update category image

---
+++ sql/REL-1.1/update_category.sql
@@ -0,0 +1,5 @@
+-- Update category image.
+INSERT INTO catalog_category_entity_varchar (attribute_id, store_id, entity_id, value) values (48, 0, 44, 'cat_44.jpg');
+INSERT INTO catalog_category_entity_varchar (attribute_id, store_id, entity_id, value) values (48, 0, 54, 'cat_54.jpg');
+INSERT INTO catalog_category_entity_varchar (attribute_id, store_id, entity_id, value) values (48, 0, 56, 'cat_56.jpg');
+
